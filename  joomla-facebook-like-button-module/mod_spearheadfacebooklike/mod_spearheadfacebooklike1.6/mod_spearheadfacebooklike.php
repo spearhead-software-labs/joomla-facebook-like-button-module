470a6cd54d3b0594d63ba68e69d21c1c260fcd0a
spearheadsoftwares <spearheadsoftwares@gmail.com>
2011-01-26 12:25:07 +0000

    Joomla 1.6 native beta version released with updated licensing. Will wait for some testing for a release candidate

---  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.6/mod_spearheadfacebooklike.php
+++  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.6/mod_spearheadfacebooklike.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Spearhead softwares Hello World! Module Entry Point
- * 
- * @package Spearhead softwares. 
- * @subpackage Modules
- * @link http://www.spearheadsoftwares.com
- * @license        GNU/GPL, see LICENSE.php
+ * @platform:Joomla 1.6
+ * @package: Spearhead softwares. 
+ * @subpackage: Modules
+ * @link: http://www.spearheadsoftwares.com
+ * @license:        GNU/GPL, see LICENSE.php
  * mod_helloworld is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
