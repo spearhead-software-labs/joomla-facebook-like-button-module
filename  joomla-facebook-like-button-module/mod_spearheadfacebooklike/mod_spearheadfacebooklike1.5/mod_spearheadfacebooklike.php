6e9002ec75aafcea26f404fa2bce919c9a5921bd
spearheadsoftwares <spearheadsoftwares@gmail.com>
2011-06-19 13:39:25 +0000

    Language stub files for rainy 3.0 Updated license information.

---  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.5/mod_spearheadfacebooklike.php
+++  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.5/mod_spearheadfacebooklike.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Spearhead softwares Hello World! Module Entry Point
+ * Spearhead softwares Joomla Facebook Module for Joomla 1.5
  * 
  * @package Spearhead softwares. 
  * @subpackage Modules
  * @link http://www.spearheadsoftwares.com
- * @license        GNU/GPL, see LICENSE.php
- * mod_helloworld is free software. This version may have been modified pursuant
+ * @license GNU/GPL, see http://www.gnu.org/copyleft/gpl.html
+ * mod_spearheadfacebooklike is free software.
+ * This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
---  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.6/mod_spearheadfacebooklike.php
+++  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.6/mod_spearheadfacebooklike.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Spearhead softwares Hello World! Module Entry Point
- * @platform:Joomla 1.6
- * @package: Spearhead softwares. 
- * @subpackage: Modules
- * @link: http://www.spearheadsoftwares.com
- * @license:        GNU/GPL, see LICENSE.php
- * mod_helloworld is free software. This version may have been modified pursuant
+ * Spearhead softwares Joomla Facebook Module for Joomla 1.6
+ * 
+ * @package Spearhead softwares. 
+ * @subpackage Modules
+ * @link http://www.spearheadsoftwares.com
+ * @license GNU/GPL, see http://www.gnu.org/copyleft/gpl.html
+ * mod_spearheadfacebooklike is free software.
+ * This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
+
 
 //no direct access
 defined('_JEXEC') or die('Restricted Access');
