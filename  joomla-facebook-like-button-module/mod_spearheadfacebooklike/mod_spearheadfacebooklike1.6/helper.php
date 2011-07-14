ab21eb998088231cfb7581e3a08161f8898e44fd
spearheadsoftwares <spearheadsoftwares@gmail.com>
2011-07-14 06:37:38 +0000

    v 3.0 initial commit

---  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.6/helper.php
+++  joomla-facebook-like-button-module/mod_spearheadfacebooklike/mod_spearheadfacebooklike1.6/helper.php
@@ -24,27 +24,107 @@ class modSpearheadFacebookLikeHelper
 	
 	public $fbLikeLinkBase = 'http://www.facebook.com/plugins/like.php?';
 	
-	
+	/**
+	 * @name getFacebookLike
+	 * @tutorial Calculates and outputs the facebook like button code
+	 * @param array $params Variable holding all the parameters of the module helper object 
+	 * @access public
+	 */	
 	public function getFacebookLike($params)
 	{
 		
-		$url =  modSpearheadFacebookLikeHelper::autoDiscovery();
+		$type = $params->get('like_button_type','iframe');
+		
+		// auto discovery is needed for iframe versions only since xfbml detects
+		// the current page and attach to the like button automatically.
+		// If explicit like_url is specified then it overrides the url taken from
+		// auto discovery or from the xfbml 
+		// $url is the final url to be attached to the like button.
+		
+		$auto_discovery = $params->get('auto_discovery');	
+		//explicit url variable.
+		$like_url = $params->get('like_url');
+		
+		if($type=='iframe')
+		{
+			if($auto_discovery=='yes' || $like_url=='')
+			{
+				$url =  modSpearheadFacebookLikeHelper::autoDiscovery();
+			}
+			else 
+			{
+				$url = $like_url;
+			}
+		}
+		elseif($type=='xfbml')
+		{
+			if($auto_discovery=='yes' || $like_url=='')
+			{
+				//set url to null as facebook will autodetect the current page.
+				//This way we reduce much markup/html taken for the url.
+				$url =  '';
+			}
+			else 
+			{
+				$url = $like_url;
+			}			
+		}
+		
+		$send_button = ($params->get('send_button')==1)?'true':'false';
+		
 		$layout = $params->get('layout_style');
 		$show_faces = ($params->get('show_faces')==1)? 'true' : 'false';
 		$width = $params->get('width');
+		$verb_to_display = $params->get('verb_to_display','like');
 		$font = $params->get('font','arial');
 		$color_scheme = $params->get('color_scheme');
-		$language = $params->get('language');
+		$language = $params->get('language','en_US');
 		$height = $params->get('height');
 		$seperator = '&amp;';		
 		
 		
-		$fbLike = '<iframe src="http://www.facebook.com/plugins/like.php?href='.$url.$seperator
-				  .'layout='.$layout.$seperator.'show_faces='.$show_faces.$seperator.'width='.$width.$seperator.'action=like&amp;font='.$font.$seperator.'colorscheme='.$color_scheme.$seperator.'height='.$height.$seperator.'language='.$language.'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$width.'px; height:'.$height.'px;" allowTransparency="true"></iframe>';
+		$document =& JFactory::getDocument();
 		
+		
+		switch($type)
+		{
+			case'iframe':
+				$fbLike = 	'<iframe src="http://www.facebook.com/plugins/like.php?href='.$url.$seperator
+				  			.'layout='.$layout.$seperator
+				  			.'show_faces='.$show_faces.$seperator
+				  			.'width='.$width.$seperator
+				  			.'action='.$verb_to_display.$seperator
+				  			.'font='.$font.$seperator
+				  			.'colorscheme='.$color_scheme.$seperator
+				  			.'height='.$height.$seperator
+				  			.'language='.$language
+				  			.'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$width.'px; height:'.$height.'px;" allowTransparency="true"></iframe>';
+				break;
+			case'xfbml':
+				$document->addScript('http://connect.facebook.net/'.$language.'/all.js#xfbml=1');
+				$fbLike = '<div id="fb-root"></div>
+						<fb:like 
+							href="'.$url.'" 
+							send="'.$send_button.'" 
+							layout="'.$layout.'" 
+							width="'.$width.'" 
+							show_faces="'.$show_faces.'" 
+							action="'.$verb_to_display.'" 
+							font="'.$font.'">
+						</fb:like>';
+				break;
+		}
+		
+						  
+				  
 		return $fbLike;
 	}
-	
+
+	/**
+	 * @name autoDiscovery
+	 * @tutorial Calculates the current url both sef and non sef urls based on joomla settings are returned
+	 * @access public
+	 */	
 	public function autoDiscovery()
 	{
 		$uri = &JURI::getInstance();
